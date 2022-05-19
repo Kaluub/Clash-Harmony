52a10e9a3a09aeec09dafe966a79c873f9f1fe66
Kaluub <kaluubs@gmail.com>
2022-05-19 00:07:59 -0400

    hotfix

--- modals/clanbattleAddQuestion.js
+++ modals/clanbattleAddQuestion.js
@@ -3,20 +3,20 @@ const Locale = require("../classes/locale.js");
 
 module.exports = {
     name: "clanbattle-add-question",
-    execute: async ({interaction, userdata}) => {
+    execute: async ({interaction}) => {
         let question = {
             title: interaction.fields.getTextInputValue("question-title"),
             answer: parseInt(interaction.fields.getTextInputValue("question-answer")),
             options: interaction.fields.getTextInputValue("question-options")
         };
-        if(isNaN(question.answer)) return {content: Locale.text(userdata.settings.locale, "CLANBATTLE_ADD_QUESTION_RESPONSE_INVALID_ANSWER_NUMBER"), ephemeral: true};
+        if(isNaN(question.answer)) return {content: Locale.text(interaction.locale, "CLANBATTLE_ADD_QUESTION_RESPONSE_INVALID_ANSWER_NUMBER"), ephemeral: true};
         question.answer -= 1;
         question.options = question.options.split("\n");
-        if(1 > question.options.length > 11) return {content: Locale.text(userdata.settings.locale, "CLANBATTLE_ADD_QUESTION_RESPONSE_INVALID_OPTIONS_AMOUNT")}
-        if(!question.options[question.answer]) return {content: Locale.text(userdata.settings.locale, "CLANBATTLE_ADD_QUESTION_RESPONSE_INVALID_ANSWER_NUMBER"), ephemeral: true};
+        if(1 > question.options.length > 11) return {content: Locale.text(interaction.locale, "CLANBATTLE_ADD_QUESTION_RESPONSE_INVALID_OPTIONS_AMOUNT")}
+        if(!question.options[question.answer]) return {content: Locale.text(interaction.locale, "CLANBATTLE_ADD_QUESTION_RESPONSE_INVALID_ANSWER_NUMBER"), ephemeral: true};
         let json = readJSON('json/clanbattle.json');
         json.questions.push(question);
         writeJSON('json/clanbattle.json', json);
-        return {content: Locale.text(userdata.settings.locale, "CLANBATTLE_ADD_QUESTION_RESPONSE_SUCCESS", question.title)};
+        return {content: Locale.text(interaction.locale, "CLANBATTLE_ADD_QUESTION_RESPONSE_SUCCESS", question.title)};
     }
 }
